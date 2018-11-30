4e488f06ecce50b8e0c6a3ec60640cb774619b66
kacper1001 <kacperkarcz1001@gmail.com>
2018-11-30 14:17:09 +0100

    Add files via upload

--- oop_dragon_1.py
+++ oop_dragon_1.py
@@ -1,22 +1,29 @@
 from random import randint
+from pprint import pprint
+
+
+class DragonConfig:
+    SMOK_MAX_HP = 100
+    SMOK_MIN_HP = 50
+    SMOK_MAX_GOLD = 100
+    SMOK_MIN_GOLD = 1
+    TEXTURE_DRAGON = 'dragon.png'
+    LIVE_POINTS = randint(SMOK_MIN_HP, SMOK_MAX_HP)
 
 
 class Dragon:
 
-    def __init__(self, smok_name, position_x, position_y, live_points, alive,
-                 texture, gold_content):
-        SMOK_MAX_HP = 100
-        SMOK_MIN_HP = 50
-        SMOK_MAX_GOLD = 100
-        SMOK_MIN_GOLD = 1
+    def __init__(self, smok_name, position_x, position_y):
 
         self.smok_name = smok_name
         self.position_x = position_x
         self.position_y = position_y
-        self.live_points = randint(SMOK_MIN_HP, SMOK_MAX_HP)
+        self.live_points= randint(DragonConfig.SMOK_MIN_HP, DragonConfig.SMOK_MAX_HP)
+        self.texture = DragonConfig.TEXTURE_DRAGON
+        self.gold_content = randint(DragonConfig.SMOK_MIN_GOLD, DragonConfig.SMOK_MAX_GOLD)
         self.alive = True
-        self.texture = "dragon.png"
-        self.gold_content = randint(SMOK_MIN_GOLD, SMOK_MAX_GOLD)
+
+
 
     def smok_attack(self):
         return (randint(5, 20))
@@ -25,37 +32,37 @@ class Dragon:
         self.alive = False
         self.texture = "dragon-dead.png"
 
-        print(f'{self.smok_name} Smok jest dead ')
-        print(f'deadnięty Smok {self.smok_name} daje {self.gold_content} złota')
+        pprint(f'{self.smok_name} Smok jest dead ')
+        pprint(f'deadnięty Smok {self.smok_name} daje {self.gold_content} złota')
 
     def smok_injured(self, injures):
         if self.alive:
             self.live_points = self.live_points - injures
 
-            print(f'Smok {self.smok_name} HP = {self.live_points}')
+            pprint(f'Smok {self.smok_name} HP = {self.live_points}')
 
             if self.live_points <= 0:
                 my_dragon.umieranie()
         else:
 
-            print(f'Smok {self.smok_name} jest already dead')
+            pprint(f'Smok {self.smok_name} jest already dead')
 
     def move(self, dela_poz_x, dela_poz_y):
 
         self.position_x = self.position_x + dela_poz_x
         self.position_y = self.position_x + dela_poz_y
 
-        print(f'Smok {self.smok_name} jest na pozycji x={self.position_x} y={self.position_y}')
+        pprint(f'Smok {self.smok_name} jest na pozycji x={self.position_x} y={self.position_y}')
 
     def set_position(self, x, y):
 
         self.position_x = x
         self.position_y = y
 
-        print(f'Smok {self.smok_name} jest na pozycji x={self.position_x} y={self.position_y} HP:{self.live_points}')
+        pprint(f'Smok {self.smok_name} jest na pozycji x={self.position_x} y={self.position_y} HP:{self.live_points}')
 
 
-my_dragon = Dragon('Wawelski', position_x=50, position_y=120)
+my_dragon = Dragon('Wawelski', 50, 120)
 my_dragon.set_position(10, 20)
 my_dragon.move(10, 20)
 my_dragon.move(-10, 0)
